1aa9b633841a4f609df07920edf8b5975d84f46c
Soumyadip Roy <soumyaroy172003@gmail.com>
2025-07-01 14:51:50 +0530

    adedd further changes

--- src/app/(protected)/dashboard/commit-log.tsx
+++ src/app/(protected)/dashboard/commit-log.tsx
@@ -4,9 +4,10 @@ import useProject from "@/hooks/use-project";
 import { api } from "@/trpc/react";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 const CommitLog = () => {
-  const { projectId } = useProject();
+  const { projectId, project } = useProject();
   const { data: commits } = api.project.getCommits.useQuery({ projectId });
   return (
     <>
@@ -30,7 +31,15 @@ const CommitLog = () => {
                 className="relative mt-4 size-8 flex-none rounded-full bg-gray-200"
               ></Image>
               <div className="rounded-mg flex-auto bg-white p-3 ring-1 ring-gray-200 ring-inset">
-                <div className="flex justify-between gap-x-4"></div>
+                <div className="flex justify-between gap-x-4">
+                  <Link
+                    target="_blank"
+                    href={`${project?.githubUrl}/commits/${commit.commitHash}`}
+                    className="py-0.5 text-xs leading-5 text-gray-500"
+                  >
+                    {commit.commitMessage}
+                  </Link>
+                </div>
               </div>
             </>
           </li>
